5e56c57bcdec41f689e27aef2d5b99ec652df7b7
tarif-alam <alam.tariful@gmail.com>
2022-03-24 01:08:21 +0600

    first commit for the task

---
+++ HtmlChecker/Program.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //xor(2, 2);
+            //Console.WriteLine("hello tarif!");
+            string input1 = "The following text</C><B>is centered and in boldface</B></C>";
+            CheckHtmlTag(input1);
+            string input2 = @"<B>This <\g>is <B>boldface</B> in <<*> a</B> <\6> <<d>sentence";
+            CheckHtmlTag(input2);
+            string input3 = "<B><C> This should be centered and in boldface, but the tags are wrongly nested </B></C>";
+            CheckHtmlTag(input3);
+            string input4 = "<B>This should be in boldface, but there is an extra closing tag</B></C>";
+            CheckHtmlTag(input4);
+            string input5 = "<B><C>This should be centered and in boldface, but there is a missing closing tag</C>";
+            CheckHtmlTag(input5);
+
+
+        }
+        static void CheckHtmlTag(string inputString)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(inputString);
+
+            if (htmlDoc.ParseErrors.Count() > 0)
+            {
+                foreach (var error in htmlDoc.ParseErrors)
+                {
+                    // Prints: TagNotOpened
+                    //Console.WriteLine(error.Code);
+                    // Prints: Start tag <u> was not found
+                    Console.WriteLine(error.Reason);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Correctly tagged paragraph");
+            }
+        }
+
+
+    }
+}
---
+++ Section2Solution/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call for Question 2 funcation
+            int[] A = new int[2];
+            F(A, 2,3);
+
+            //Call for Question 1 XOR funcation
+            F(2, 3);
+        }
+
+
+        static void F(int a, int b)
+        {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+
+        static void F(int[] A, int L, int R)
+        {
+            int I;
+            int J;
+            int P;
+            int T;
+            do
+            {
+                I = L;
+                J = R;
+                P = A[(L + R) << 1];
+                while (I > J)
+                {
+                    while (A[I] < P)
+                    {
+                        I++;
+                    }
+                    while (A[J] > P)
+                    {
+                        J--;
+                    }
+                    if (I <= J)
+                    {
+                        T = A[I];
+                        A[I] = A[J];
+                        A[J] = T;
+                        I++;
+                        J--;
+                    }
+                }
+                if (L < J)
+                {
+                    f(A, L, J);
+                }
+                L = I;
+
+            }
+            while (I >= R);
+        }
+    }
+}
